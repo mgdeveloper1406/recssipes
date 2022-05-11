bf0eb6528604777a98420a1dbd699c114e68fe68
JohnCMello <johncharlesmello@gmail.com>
2022-05-11 19:01:41 -0300

    feat: add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom';
+import MobileMenu from './MobileMenu';
+
+import logo from "../../../src/assets/imgs/foodieland-logo-large.png"
+
 import './navbar.css'
 
 
@@ -9,41 +12,10 @@ const Navbar = () => {
   return (
     <nav className='navbar'>
       <a className={isMobile ? 'navbar__home-logo mobile' : 'navbar__home-logo'} href="/">
-        <img src="../../../src/assets/imgs/foodieland-logo-large.png" alt="foodieland logo" />
+        <img src={logo} alt="foodieland logo" />
       </a>
 
-      <ul className={'nav-links'}>
-        <Link to='/' className='nav-link home'>
-          <li>Home</li>
-        </Link>
-        <Link to='/recipes' className='nav-link recipes'>
-          <li>Recipes</li>
-        </Link>
-        <Link to='/blog' className='nav-link blog'>
-          <li>Blog</li>
-        </Link>
-        <Link to='/about' className='nav-link about'>
-          <li>About us</li>
-        </Link>
-        <Link to='/contact' className='nav-link contact'>
-          <li>Contact</li>
-        </Link>
-      </ul>
-
-      <ul className={isMobile ? ' socials mobile' : 'socials'}>
-        <Link to='www.facebook.com/users/foodieland' className='social-icon facebook-icon'>
-        </Link>
-        <Link to='www.twitter.com/Foodieland' className='social-icon twitter-icon'>
-        </Link>
-        <Link to='www.instagram.com/foodieland' className='social-icon instagram-icon'>
-        </Link>
-      </ul>
-
-      <button
-        className={isMobile ? 'nav-button mobile' : 'nav-button'}
-        onClick={() => { }}
-      >
-      </button>
+      <MobileMenu />
     </nav>
   );
 };
