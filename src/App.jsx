7aeb86034237f86932bed9ad4e70598687f28558
JohnCMello <johncharlesmello@gmail.com>
2022-05-11 16:30:57 -0300

    start navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home'
+import Recipes from './pages/Recipes'
+import Blog from './pages/Blog'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
 import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div >
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} exact />
+        <Route path='/recipes' element={<Recipes />} exact />
+        <Route path='/blog' element={<Blog />} exact />
+        <Route path='/about' element={<About />} exact />
+        <Route path='/contact' element={<Contact />} exact />
+      </Routes>
+
     </div>
   )
 }
 
-export default App
+
+export default App;
