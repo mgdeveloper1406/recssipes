6e98beded95aae7948fd7ccf8882a200150b8188
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-05-12 16:49:45 -0300

    added mobile carousel and recipe cards

---
+++ src/components/ContactUsRecipes.jsx
@@ -0,0 +1,37 @@
+import img1 from '../../public/images/recipe1.png'
+import img2 from '../../public/images/recipe2.png'
+import img3 from '../../public/images/recipe3.png'
+import img4 from '../../public/images/recipe4.png'
+
+const ContactUsRecipes = [
+  {
+    id: '1',
+    image: img1,
+    title: 'Mixed Tropical Fruit Salad with Superfood Boosts',
+    tag1: '30 minutes',
+    tag2: 'Healthy'
+  },
+  {
+    id: '2',
+    image: img2,
+    title: 'Big and Juicy Wagyu Beef Cheeseburger',
+    tag1: '30 minutes',
+    tag2: 'Western'
+  },
+  {
+    id: '3',
+    image: img3,
+    title: 'Healthy Japanese Fried Rice with Asparagus',
+    tag1: '30 minutes',
+    tag2: 'Healthy'
+  },
+  {
+    id: '4',
+    image: img4,
+    title: 'Cauliflower Walnut Vegetarian Taco Meat',
+    tag1: '30 minutes',
+    tag2: 'Eastern'
+  },
+]
+
+export default ContactUsRecipes
