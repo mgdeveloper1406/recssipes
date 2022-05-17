285acd4ffc9bdfa88039f941c53054bc1f4944e1
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-05-17 17:39:10 -0300

    added form jsx

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,28 @@
+const Input = ({ 
+  labelText, 
+  id, 
+  type, 
+  placeholder, 
+  name, 
+  value, 
+  handleOnChange, 
+  required = false
+}) => {
+
+  return (
+    <div>
+      <label htmlFor={id}>{labelText}</label>
+      <input
+        id={id} 
+        type={type}
+        placeholder={placeholder} 
+        name={name} 
+        value={value} 
+        onChange={handleOnChange} 
+        required={required}
+      />
+    </div>
+  )
+}
+ 
+export default Input
