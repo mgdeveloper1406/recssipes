3e1d213a4aa69182f50c6541156496000c14b03f
JohnCMello <johncharlesmello@gmail.com>
2022-05-17 11:14:08 -0300

    change icons import

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,40 +1,43 @@
-import { Link } from 'react-router-dom'
-import TimerIcon from '../../../public/icons/timer.svg'
-import ForkKnifeIcon from '../../../public/icons/forkKnife.svg'
-import './RecipeCard.css'
-import LikeButton from '../LikeButton/LikeButton'
+import { Link } from 'react-router-dom';
 
+import LikeButton from '../LikeButton/LikeButton';
+import TimerIcon from '/icons/timer.svg';
+import ForkKnifeIcon from '/icons/forkKnife.svg';
+
+import './RecipeCard.css';
+
+// id received in props but never used
 const RecipeCard = ({ id, image, title, tag1, tag2 }) => {
 
   return (
-      <li className='recipe-card__container'>
-        <Link to='/' className='recipe-card__link'>
-          <div className='recipe-card'>
-            <img className='recipe-card__img' src={image} alt={title} />
-            <LikeButton />
-            <h3 className='recipe-card__title'>{title}</h3>
-            <div className='recipe-card__tags'>
-              <span className='recipe-card__tag1'>
-                <img 
-                  src={TimerIcon} 
-                  alt='Timer' 
-                  className='recipe-card__tag1--icon'
-                /> 
-                {tag1}
-              </span>
-              <span className='recipe-card__tag2'>
-                <img 
-                  src={ForkKnifeIcon} 
-                  alt='Fork and knife' 
-                  className='recipe-card__tag2--icon'
-                /> 
-                {tag2}
-              </span>
-            </div>
+    <li className='recipe-card__container'>
+      <Link to='/' className='recipe-card__link'>
+        <div className='recipe-card'>
+          <img className='recipe-card__img' src={image} alt={title} />
+          <LikeButton />
+          <h3 className='recipe-card__title'>{title}</h3>
+          <div className='recipe-card__tags'>
+            <span className='recipe-card__tag1'>
+              <img
+                src={TimerIcon}
+                alt='Timer'
+                className='recipe-card__tag1--icon'
+              />
+              {tag1}
+            </span>
+            <span className='recipe-card__tag2'>
+              <img
+                src={ForkKnifeIcon}
+                alt='Fork and knife'
+                className='recipe-card__tag2--icon'
+              />
+              {tag2}
+            </span>
           </div>
-        </Link>
-      </li>
-   );
-}
- 
+        </div>
+      </Link>
+    </li>
+  );
+};
+
 export default RecipeCard;
