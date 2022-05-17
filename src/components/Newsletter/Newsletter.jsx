9992b70393fcefdc73752cb038494750b239039f
JohnCMello <johncharlesmello@gmail.com>
2022-05-17 11:17:55 -0300

    change card tittle

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -7,8 +7,8 @@ const Newsletter = () => {
     <div className={`newsletter-container`}>
       <form className={`newsletter__form`}>
         <header className={`header-container`}>
-          <h2 className={`newsletter__title`}>Mussum Ipsum, cacilds vidis litro abertis.</h2>
-          <p className={`newsletter__paragraph`}>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
+          <h2 className={`newsletter__title`}>Deliciousness to your inbox</h2>
+          <p className={`newsletter__paragraph`}>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor</p>
         </header>
         <div className={`input-container`}>
           <input className={`newsletter__input`} type="email" placeholder='Your email adress...' />
