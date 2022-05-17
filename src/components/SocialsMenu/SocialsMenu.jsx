d9d8cc048fb6d2f4a0e2e93276d526cc8a6c49b5
JohnCMello <johncharlesmello@gmail.com>
2022-05-17 18:38:56 -0300

    change classes to render element based on media size

--- src/components/SocialsMenu/SocialsMenu.jsx
+++ src/components/SocialsMenu/SocialsMenu.jsx
@@ -1,14 +1,35 @@
 import { Link } from 'react-router-dom'
+import fbLogo from '/public/icons/001-facebook.svg'
+import twitterLogo from '/public/icons/003-twitter.svg'
+import instagramLogo from '/public/icons/instagram.svg'
 
 const SocialsMenu = () => {
   return (
-    <ul className={isMobile ? ' socials mobile' : 'socials'}>
-      <Link to='www.facebook.com/users/foodieland' className='social-icon facebook-icon'>
-      </Link>
-      <Link to='www.twitter.com/Foodieland' className='social-icon twitter-icon'>
-      </Link>
-      <Link to='www.instagram.com/foodieland' className='social-icon instagram-icon'>
-      </Link>
+    <ul className='social-list'>
+      <li className='social-list-item'>
+        <Link
+          to='/facebook'
+          className='social-link'
+        >
+          <img src={fbLogo} alt="facebook logo" />
+        </Link>
+      </li>
+      <li>
+        <Link
+          to='/twitter'
+          className='social-icon'
+        >
+          <img src={twitterLogo} alt="twitter logo" />
+        </Link>
+      </li>
+      <li>
+        <Link
+          to='/instagram'
+          className='social-icon'
+        >
+          <img src={instagramLogo} alt="instagram logo" />
+        </Link>
+      </li>
     </ul>
   )
 }
