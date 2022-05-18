ccadbce821738abf59bfd05bc190b885f62b629d
Gabianchini <gabibisdx@gmail.com>
2022-05-18 17:02:33 -0300

    icons footer responsivos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,21 +13,31 @@ function Footer() {
   return (
     <div className='footer-container'>
         <footer className='footer'>
+        <div className='icon-text'>
           <div className='teste1'>
           <img className='icone'src={logo}/>
+          <div className='text-footer'>
           <p>Testinho br testando o teste do teste da testa testado</p>
           </div>
-          <div className='teste2'>
-          <a>Recipes</a> <a>Blog</a> <a>Contact</a>  <a>About us</a>
           </div>
+          <div className='teste2'>
+          <a className='link-footer'>Recipes</a> <a className='link-footer' >Blog</a> <a className='link-footer' >Contact</a>  <a className='link-footer'>About us</a>
+          </div>
+      </div>
           <hr></hr>
           <div>
           <p className='copy'> &copy;2020 Flowbase.Powered by <span className='webflow'>Webflow</span></p>
           </div>
-          <div className='icons'>
-          <a><img className="face-icon"src={face}/></a> 
-          <a><img className="twitter-icon" src={twitter}/></a>
-          <a><img className="insta-icon" src={instagram}/></a>
+          <div className='icons'> 
+          <a className='links-footer-icon'>
+          <img className="face-icon"src={face}/>
+          </a> 
+          <a className='links-footer-icon'>
+          <img className="twitter-icon" src={twitter}/>
+          </a>
+          <a className='links-footer-icon'>
+          <img className="insta-icon" src={instagram}/>
+          </a>
           </div>
           </footer>
    </div>
