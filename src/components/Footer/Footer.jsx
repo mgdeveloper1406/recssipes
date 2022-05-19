37f3c6c4f385d07250438da69d1299c35bd6d378
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-05-19 15:17:21 -0300

    additional fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
         <div className='icon-text'>
           <div className='teste1'>
           <img className='icone'src={logo}/>
-          <p>Testinho br testando o teste do teste da testa testado</p>
+          <p className='footer-text'>Testinho br testando o teste do teste da testa testado</p>
           </div>
           <div className='teste2'>
           <a className='link-footer'>Recipes</a> <a className='link-footer' >Blog</a> <a className='link-footer' >Contact</a>  <a className='link-footer'>About us</a>
@@ -24,7 +24,7 @@ function Footer() {
       </div>
           <hr></hr>
           <div className='copyr'>
-          <p className='copy'> &copy;2020 Flowbase.Powered by <span className='webflow'>Webflow</span></p>
+          <p className='footer-text copy'> &copy;2020 Flowbase.Powered by <span className='webflow'>Webflow</span></p>
           </div> 
           <div className='icons'> 
           <a className='links-footer-icon'>
