4695daa7985d2cbb253bad051b10c68892da51e2
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-05-19 16:14:43 -0300

    newsletter fixes

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -9,11 +9,11 @@ const Newsletter = () => {
         <form className={`newsletter__form`}>
           <header className={`header-container`}>
             <h2 className={`newsletter__title`}>Deliciousness to your inbox</h2>
-            <p className={`newsletter__paragraph`}>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor</p>
+            <p className={`newsletter__paragraph`}>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim</p>
           </header>
           <div className={`input-container`}>
             <input className={`newsletter__input`} type="email" placeholder='Your email adress...' />
-            <button className={`newsletter__button`} >Subscribet</button>
+            <button className={`newsletter__button`} >Subscribe</button>
           </div>
         </form>
       </div>
