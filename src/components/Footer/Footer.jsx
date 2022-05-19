436635805f865f2e7d217f5827aea55f1d2592ee
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-05-19 15:19:53 -0300

    resolved conflict

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,32 +9,6 @@ function Footer() {
 
   return (
     <div className='footer-container'>
-<<<<<<< HEAD
-      <footer className='footer'>
-        <div className='teste1'>
-          <img className='icone' src={logo} />
-          <p className='texto'>Testinho br testando o teste do teste da testa testado</p>
-        </div>
-        <div className='teste2'>
-          <a>Recipes</a>
-          <a>Blog</a>
-          <a>Contact</a>
-          <a>About us</a>
-        </div>
-        <hr></hr>
-        <div>
-          <p className='copy'> &copy;2020 Flowbase.Powered by <span className='webflow'>Webflow</span></p>
-        </div>
-        <div className='icons'>
-          <a><img className="face-icon" src={face} /></a>
-          <a><img className="twitter-icon" src={twitter} /></a>
-          <a><img className="insta-icon" src={instagram} /></a>
-        </div>
-      </footer>
-    </div>
-  );
-};
-=======
         <footer className='footer'>
         <div className='icon-text'>
           <div className='teste1'>
@@ -68,7 +42,6 @@ function Footer() {
     
   )
 }
->>>>>>> fix-footer
 
 
 export default Footer;
