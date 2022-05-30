2391a7c77eadb08a7006453e2b59136497a5a081
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-05-30 18:13:59 -0300

    added picture tag to advertising card

--- src/components/AdvertisingCard/index.jsx
+++ src/components/AdvertisingCard/index.jsx
@@ -4,8 +4,11 @@ import './styles.scss'
 
 const AdvertisingCard = ({ data }) => {
   return (
-    <Link to={data.linkPath} className='advertisingCard'>
-      <img className='advertisingCard__img' src={data.img} alt={data.alt}></img>
+    <Link to={data.linkPath} className='advertisingCard__link'>
+      <picture className='advertisingCard'>
+        <source srcSet={data.imgSmall} media='(max-width: 450px)' />
+        <img className='advertisingCard__img' src={data.img} alt={data.alt} />
+      </picture>
     </Link>
   )
 }
--- src/components/AdvertisingData.js
+++ src/components/AdvertisingData.js
@@ -2,6 +2,7 @@ const AdvertisingData = {
   id: '1',
   linkPath: '/',
   img: '/imgs/ad-card.png',
+  imgSmall: 'imgs/ad-card-small.png',
   alt: "Advertisement – Don't forget to eat healthy food! Click here to go to foodieland.com"
 }
 
