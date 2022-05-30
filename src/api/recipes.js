deb0625010d8c4b61c400628342e28cb859aaa70
JohnCMello <johncharlesmello@gmail.com>
2022-05-30 15:50:28 -0300

    add mock api

---
+++ src/api/recipes.js
@@ -0,0 +1,72 @@
+import picture1 from '/public/chicken-ramen-soup.png'
+import picture2 from '/public/chicken-ramen-soup.png'
+import picture3 from '/public/chicken-ramen-soup.png'
+import picture4 from '/public/chicken-ramen-soup.png'
+import picture5 from '/public/chicken-ramen-soup.png'
+import picture6 from '/public/chicken-ramen-soup.png'
+
+import user1 from '/public/wade-warren.png'
+import user2 from '/public/robert-fox.png'
+import user3 from '/public/dianne-russel.png'
+import user4 from '/public/leslie-alexander.png'
+import user5 from '/public/courtney-henry.png'
+import user6 from '/public/albert-flores.png'
+
+
+
+export const articleList = [
+  {
+    id: '1',
+    image: picture1,
+    title: 'Crochet Project for Noodle Lovers',
+    description: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.',
+    userImage: user1,
+    userName: 'Wade Warren',
+    publushedAt: '12 November 2021'
+  },
+  {
+    id: '2',
+    image: picture2,
+    title: '10 Vegetarian Recipes To Eat This Month ',
+    description: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.',
+    userImage: user2,
+    userName: 'Robert Fox',
+    publushedAt: '12 November 2021'
+  },
+  {
+    id: '3',
+    image: picture3,
+    title: 'Full Guide to Becoming a Professional Chef',
+    description: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.',
+    userImage: user3,
+    userName: 'Dianne Russel',
+    publushedAt: '12 November 2021'
+  },
+  {
+    id: '4',
+    image: picture4,
+    title: 'Simple & Delicious Vegetarian Lasagna',
+    description: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.',
+    userImage: user4,
+    userName: 'Leslie Alexander',
+    publushedAt: '12 November 2021'
+  },
+  {
+    id: '5',
+    image: picture5,
+    title: 'Platain and Pinto Stew with Aji Verde',
+    description: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.',
+    userImage: user5,
+    userName: 'Courtney Henry',
+    publushedAt: '12 November 2021'
+  },
+  {
+    id: '6',
+    image: picture6,
+    title: 'We\'re Hiring a Communications Assistant',
+    description: 'Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.',
+    userImage: user6,
+    userName: 'Albert Flores',
+    publushedAt: '12 November 2021'
+  }
+];
