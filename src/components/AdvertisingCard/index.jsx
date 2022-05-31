da06d0fef29e309408e0bbb359e11ed1870b19dc
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-05-31 10:27:10 -0300

    added target and div

--- src/components/AdvertisingCard/index.jsx
+++ src/components/AdvertisingCard/index.jsx
@@ -4,12 +4,14 @@ import './styles.scss'
 
 const AdvertisingCard = ({ data }) => {
   return (
-    <Link to={data.linkPath} className='advertisingCard__link'>
-      <picture className='advertisingCard'>
-        <source srcSet={data.imgSmall} media='(max-width: 450px)' />
-        <img className='advertisingCard__img' src={data.img} alt={data.alt} />
-      </picture>
-    </Link>
+    <div className='advertisingCard'>
+      <Link to={data.linkPath} target='_blank' className='advertisingCard__link'>
+        <picture>
+          <source srcSet={data.imgSmall} media='(max-width: 450px)' />
+          <img className='advertisingCard__img' src={data.img} alt={data.alt} />
+        </picture>
+      </Link>
+    </div>
   )
 }
 
