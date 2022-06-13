45090e39afb53b6ff12fc59b383a3d0b315366a8
Gabianchini <gabibisdx@gmail.com>
2022-06-13 17:23:27 -0300

    new html div container for footer added

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,29 +7,29 @@ import './styles.scss'
 function Footer() {
 
   return (
-    <div className='footer__container'> {/* footer-container*/}
+    <div className='footer__container'>
         <footer className='footer'>
-        <div className='footer__header'>{/* icon-text*/}
-          <div className='footer__headerInfo'> {/* teste1*/}
-            <img className='footer__logo'src={logo}/>{/* icon*/}
-            <p className='footer__text'>Lorem ipsum dolor sit amet, consectetuipisicing elit,</p>{/* footer-text*/}
+        <div className='footer__header'>
+          <div className='footer__headerInfo'> 
+            <img className='footer__logo'src={logo}/>
+            <p className='footer__text'>Lorem ipsum dolor sit amet, consectetuipisicing elit,</p>
           </div>
           <div className='footer__links'>{/* teste2*/}
             <Link to='/recipes' className='footer__link'>Recipes</Link> 
             <Link to='/blog' className='footer__link'>Blog</Link>
             <Link to='/contact' className='footer__link' >Contact</Link> 
-            <Link to='/about' className='footer__link'>About us</Link>{/*link-footer ou a */} 
+            <Link to='/about' className='footer__link'>About us</Link>
           </div>
         </div>
           <hr className='footer__horizontalRule'></hr>
-          <div className='footer__copyright__container'>{/* copyr*/}
-          <p className='footer__copyright'> &copy;2020 Flowbase.Powered by <span className='footer__copyrightWebflow'>Webflow</span></p>{/* copy e span*/}
-          </div> 
-          <div className='footer__socials'> {/* icons*/}
+          <div className='footer__socialsContainer'>
+          <p className='footer__copyright'> &copy;2020 Flowbase.Powered by <span className='footer__copyrightWebflow'>Webflow</span></p>
+          <div className='footer__socials'>
           <SocialsMenu />
           </div>
+          </div>
           </footer>
-   </div>
+          </div>
 
        
 
