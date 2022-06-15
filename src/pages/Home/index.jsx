c8e5f0bec3b4436100c03ea893cc8c639c41eef5
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-06-15 11:32:31 -0300

    added try out recipes section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import Footer from '../../components/Footer'
+import Newsletter from '../../components/Newsletter'
 import RecipeCategories from '../../components/RecipeCategories/index.jsx'
+import TryOutRecipes from '../../components/TryOutRecipes/index.jsx'
+import TryOutRecipesData from '../../api/TryOutRecipes.js'
 
 import './styles.scss'
 
 const Home = () => {
   return (
     <div className='home-page__container'>
+      {/* Hero section */}
       <RecipeCategories />
+      {/* Simple and Tasty Recipes section */}
+      {/* Everyone can be a chef... section */}
+      {/* Check out @foodieland on Instagram section */}
+      <TryOutRecipes recipes={TryOutRecipesData}/>
+      <Newsletter />
+      <Footer />
     </div>
   )
 }
