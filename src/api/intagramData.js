4e5cb25634f7f4ef8c5c4d5e696fe847ccf1f631
John Mello <johncharlesmello@gmail.com>
2022-06-15 16:44:42 -0300

    create instagram api

---
+++ src/api/intagramData.js
@@ -0,0 +1,26 @@
+import instagram_1 from '/imgs/post-1.png'
+import instagram_2 from '/imgs/post-2.png'
+import instagram_3 from '/imgs/post-3.png'
+import instagram_4 from '/imgs/post-4.png'
+
+
+const instagramData = [
+  {
+    id: 1,
+    image: instagram_1
+  },
+  {
+    id: 2,
+    image: instagram_2
+  },
+  {
+    id: 3,
+    image: instagram_3
+  },
+  {
+    id: 3,
+    image: instagram_4
+  }
+]
+
+export default instagramData;
