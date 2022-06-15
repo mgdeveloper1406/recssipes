9410e127b6d89a07e1f1866b8ecad0ece506dcdf
John Mello <johncharlesmello@gmail.com>
2022-06-15 17:57:44 -0300

    change import path to alias @api - add classNames

--- src/components/InstagramSection/index.jsx
+++ src/components/InstagramSection/index.jsx
@@ -1,20 +1,23 @@
 import Carousel from '@components/Carousel'
 import InstagramCard from '@components/InstagramCard';
 
-import instagramData from '@api/intagramData';
-import ArrowIcon from "/icons/arrow.svg";
-import './styles.css'
+import ArrowIcon from '/icons/arrow.svg';
+import InstagramIcon from '/icons/instagram.svg'
 
-const IntagramSection = () => {
+import './styles.scss'
+
+const InstagramSection = ({ posts }) => {
   return (
-    <section>
-      <h2>Check out @foodieland on <a href="www.instagram.com/foofieland">Instagram</a></h2>
+    <section className='instagramSection'>
+      <h2 className='instagramSection__title'>Check out @foodieland on <a className='instagramSection__titleLink' href="www.instagram.com/foofieland">Instagram</a></h2>
+      <p className='instagramSection__paragraph'></p>
       <Carousel
-        itemsArray={instagramData}
+        itemsArray={posts}
         ItemComponent={InstagramCard}
         arrowIcon={ArrowIcon}
       />
+      <button className='instagramSection__button'>Vist our Instagram</button>
     </section>
   )
 }
-export default IntagramSection
+export default InstagramSection
