5e25f9b2dfb8f4ff422573237d64d93d7a8ff6dc
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-06-24 09:29:32 -0300

    added input label to newsletter input

--- src/components/organisms/Newsletter/index.jsx
+++ src/components/organisms/Newsletter/index.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import { useRef } from 'react'
 
 import './styles.scss'
 
 const Newsletter = () => {
+
+  const inputRef = useRef()
+
   return (
     <div className={`newsletter-container`}>
       <div className={`newsletter`}>
@@ -12,14 +15,21 @@ const Newsletter = () => {
             <p className={`newsletter__paragraph`}>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim</p>
           </header>
           <div className={`input-container`}>
-            <input className={`newsletter__input`} type="email" placeholder='Your email adress...' />
+            <label htmlFor='newsletterEmail' className='newsletter__inputLabel--visuallyHidden'>Email</label>
+            <input
+              ref={inputRef}
+              className={`newsletter__input`} 
+              type='email' 
+              placeholder='Your email adress...'
+              name='userEmail'
+            />
             <button className={`newsletter__button`} >Subscribe</button>
           </div>
         </form>
       </div>
 
     </div>
-  );
-};
+  )
+}
 
-export default Newsletter;
+export default Newsletter
