aa7d9f218d9fccd2493806939ff806ebbb935df7
Isabelly Monteiro <isabelly.monteiro@valtech.com>
2022-07-21 18:04:03 -0300

    removed hero section

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -4,7 +4,6 @@ import RecipeCategories from '@organisms/RecipeCategories'
 import SimpleTastyRecipes from '@organisms/SimpleTastyRecipes'
 import TryOutRecipes from '@organisms/TryOutRecipes'
 import InstagramSection from '@organisms/InstagramSection'
-import Hero from '@organisms/HeroSection'
 import RecipesData from '@api/RecipesData.json'
 import instagramData from '@api/intagramData.js';
 
@@ -13,7 +12,6 @@ import './styles.scss'
 const Home = () => {
   return (
     <div className='homePage'>
-      <Hero/>
       <RecipeCategories />
       <SimpleTastyRecipes recipes={RecipesData.slice(4, 12)} />
       <LearnMore />
